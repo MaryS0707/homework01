08e6924c4aff82a35f185c633d226cef0b0edfac
Mary <sokolova.mariya.2000@bk.ru>
2022-12-08 02:17:51 +0700

    Add task 8

---
+++ task8/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int GetNumber (string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+    {
+        Console.Write(message);
+        if((int.TryParse(Console.ReadLine(), out result)) && result > 1)
+        {
+            return result;
+        }
+        else
+            Console.WriteLine("Ввели не число/не подходящее число");
+    }
+    return result;
+}
+
+int num = GetNumber("Введите число: ");
+Console.Write(num + " -> ");
+
+for (int i = 2; i <= num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+ 
+}
